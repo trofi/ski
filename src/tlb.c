trofi/ski

commit 3279e9defb3b2a2c7cc76818abc4c80aadbb9343

src/tlb.c: fix `Accesstype` vs `Accesstypemask` confsion
Noticed by `clang`:

```
ski> tlb.c:698:16: warning: comparison of different enumeration types ('Accesstype' and 'Accesstypemask') [-Wenum-compare]
ski>   698 |             if (atype != READ_ACCESS || cpl > pl)
ski>       |                 ~~~~~ ^  ~~~~~~~~~~~
ski> tlb.c:734:16: warning: comparison of different enumeration types ('Accesstype' and 'Accesstypemask') [-Wenum-compare]
ski>   734 |             if (atype == READ_ACCESS && cpl)
ski>       |                 ~~~~~ ^  ~~~~~~~~~~~
```

THe code should nto change, but it should be less confusing.

src/tlb.c
@@ -691,15 +691,15 @@ static int keyCheck(Accesstype atype, unsigned key)
 static BOOL accessRights(unsigned ar, unsigned pl, unsigned cpl,
 			 Accesstype atype)
 {
-    atype &= READ_ACCESS | WRITE_ACCESS;
+    Accesstypemask atypem = atype & READ_ACCESS | WRITE_ACCESS;
 
     switch (ar) {
 	case 0:
-	    if (atype != READ_ACCESS || cpl > pl)
+	    if (atypem != READ_ACCESS || cpl > pl)
 		return NO;
 	    break;
 	case 1:
-	    if (atype & WRITE_ACCESS || cpl > pl)
+	    if (atypem & WRITE_ACCESS || cpl > pl)
 		return NO;
 	    break;
 	case 2:
@@ -713,13 +713,13 @@ static BOOL accessRights(unsigned ar, unsigned pl, unsigned cpl,
 	case 4:
 	    if (atype == EXECUTE_ACCESS || cpl > pl)
 		return NO;
-	    if ((atype & WRITE_ACCESS) && cpl && cpl == pl)
+	    if ((atypem & WRITE_ACCESS) && cpl && cpl == pl)
 		return NO;
 	    break;
 	case 5:
 	    if (cpl > pl)
 		return NO;
-	    if ((atype & WRITE_ACCESS) && cpl)
+	    if ((atypem & WRITE_ACCESS) && cpl)
 		return NO;
 	    break;
 	case 6:
@@ -729,9 +729,9 @@ static BOOL accessRights(unsigned ar, unsigned pl, unsigned cpl,
 		return NO;
 	    break;
 	case 7:
-	    if (atype & WRITE_ACCESS)
+	    if (atypem & WRITE_ACCESS)
 		return NO;
-	    if (atype == READ_ACCESS && cpl)
+	    if (atypem == READ_ACCESS && cpl)
 		return NO;
 	    break;
     }

commit 735dab32480f58d0c53c2e12ade07067e91981b5

src/tlb.c: restore original order of operation for atype assignment
After 3279e9defb3b trying to boot a Linux kernel in (b)ski's system mode
hang early.

As `&` takes precedence over `|` the assigned value of atypem is different
after 3279e9defb3b than the assigned value of atype was before that commit.

Fixes: 3279e9defb3b ("src/tlb.c: fix `Accesstype` vs `Accesstypemask` confsion")
Reported-by: Willow <prometrium.dev@proton.me>
Signed-off-by: Johnny Mnemonic <jm@machine-hall.org>

src/tlb.c
@@ -691,7 +691,7 @@ static int keyCheck(Accesstype atype, unsigned key)
 static BOOL accessRights(unsigned ar, unsigned pl, unsigned cpl,
 			 Accesstype atype)
 {
-    Accesstypemask atypem = atype & READ_ACCESS | WRITE_ACCESS;
+    Accesstypemask atypem = atype & (READ_ACCESS | WRITE_ACCESS);
 
     switch (ar) {
 	case 0: